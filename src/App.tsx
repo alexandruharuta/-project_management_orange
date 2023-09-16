b62db421b06cc74356721bfd2698425bb79b0792
alexandruharuta <alexandruharuta@outlook.com>
2023-09-16 23:15:20 +0300

    Nabar and Sidebar Components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Button from './Components/Button'
+import Layout from './Components/Layout/Layout'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
       <h1 className='text-3xl font-bold underline'>Hello 2</h1>
       <Button className='bg-sky-500/75 px-3 py-2 text-sky-50 rounded-lg'>Button</Button>
-
+      <Layout></Layout>
     </>
   )
 }
---
+++ src/Components/Layout/Layout.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar"
+import Sidebar from "./Sidebar"
+
+export default function Layout() {
+    return (
+        <>
+            <Navbar></Navbar>
+            <Sidebar></Sidebar>
+        </>
+    )
+}
