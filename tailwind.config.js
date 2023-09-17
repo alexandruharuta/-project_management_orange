166f6dea239319df2c0253bc074cb2f14f0c89ef
CValeria <124835561+CValeria@users.noreply.github.com>
2023-09-17 20:36:14 +0300

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'dark': '#131927',
+      transparent: "transparent",
+      dark: "#131927",
+      current: "currentColor",
+      blue: "#CFE0F0",
     },
   },
   plugins: [],
-}
-
+};
