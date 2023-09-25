580df16f14c31a5bfa96c1a3b004c32a4162056c
Țurcanu Dorin <93262227+turcanud@users.noreply.github.com>
2023-09-25 20:14:21 +0300

    toggle add button

--- src/Pages/Project.tsx
+++ src/Pages/Project.tsx
@@ -4,9 +4,11 @@ import AddTask from "../Components/TasksComps/AddTask";
 
 const Project = () => {
   const [tasks, setTasks] = useState([])
+  const [showForm, setShowForm] = useState(false)
   return (
-    <div className="px-16 py-16">
-      <AddTask/>
+    <div className="px-16 py-16 overflow-hidden">
+      <button className={`rounded-full outline-none ${showForm ? 'bg-red-700' : 'bg-green-700'} px-5 py-3 mb-2 text-white font-semibold box-border`} onClick={() => setShowForm(!showForm)}>{!showForm ? 'Add Task':'Close'}</button>
+      {showForm && <AddTask/>}
       <Tasks/>
     </div>
   )
