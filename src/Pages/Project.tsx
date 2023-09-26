0ea284ae0449b9f8bc8248f89127bbbdc22291a6
alexandruharuta <alexandruharuta@outlook.com>
2023-09-26 14:42:17 +0300

    .

--- src/Pages/Project.tsx
+++ src/Pages/Project.tsx
@@ -2,7 +2,7 @@ import Tasks from "../Components/TasksComps/Tasks"
 import { useState } from 'react';
 import AddTask from "../Components/TasksComps/AddTask";
 
-interface  ITask{
+interface ITask {
   id: number;
   taskName: string;
   dueDate: Date;
@@ -13,7 +13,7 @@ const Project = () => {
   const [showForm, setShowForm] = useState(false)
   const [tasks, setTasks] = useState<ITask[]>([])
 
-  const addTask = (task:any) => {
+  const addTask = (task: any) => {
     const id = Math.floor(Math.random() * 10000) + 1
     const newTask = { id, ...task }
     setTasks([...tasks, newTask])
@@ -29,11 +29,12 @@ const Project = () => {
 
   return (
     <div className="px-16 py-16 overflow-hidden">
-      <button className={`rounded-full outline-none ${showForm ? 'bg-red-700 hover:bg-red-800' : 'bg-green-700 hover:bg-green-800'} px-5 py-3 mb-2 text-white font-semibold box-border ease-in-out duration-500`} onClick={() => setShowForm(!showForm)}>{!showForm ? 'Add Task':'Close'}</button>
+      <button className={`rounded-full outline-none ${showForm ? 'bg-red-700 hover:bg-red-800' : 'bg-green-700 hover:bg-green-800'} px-5 py-3 mb-2 text-white font-semibold box-border ease-in-out duration-500`} onClick={() => setShowForm(!showForm)}>{!showForm ? 'Add Task' : 'Close'}</button>
       {showForm && <AddTask addTask={addTask} />}
-      {tasks.length > 0 ? <Tasks tasks={tasks}  removeOnComplete = {removeOnComplete}/> : <h1 className="font-semibold mt-4">!No tasks to show!</h1>}
+      {tasks.length > 0 ? <Tasks tasks={tasks} removeOnComplete={removeOnComplete} /> : <h1 className="font-semibold mt-4">!No tasks to show!</h1>}
     </div>
   )
 }
+
 
 export default Project
