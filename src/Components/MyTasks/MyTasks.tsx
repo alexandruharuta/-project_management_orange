d4e51125cf23c9ade51adb9b73ec25c4de46d66e
Țurcanu Dorin <93262227+turcanud@users.noreply.github.com>
2023-10-01 17:15:18 +0300

    search bar

--- src/Components/MyTasks/MyTasks.tsx
+++ src/Components/MyTasks/MyTasks.tsx
@@ -2,9 +2,12 @@ import {useEffect, useState} from "react";
 import {IProject} from "../../api/interfaces/IProject";
 import TableRow from "./TableRow";
 import api from "../../api/mockApi";
+import {FaSearch} from "react-icons/fa";
 
 const MyTasks = () => {
   const [projects, setProjects] = useState<IProject[]>([]);
+  const [input, setInput] = useState("");
+
   useEffect(() => {
     (async () => {
       setProjects(await api.getAllProjects());
@@ -14,6 +17,20 @@ const MyTasks = () => {
   return (
     <>
       <p className="font-medium text-2xl ml-5 mt-3">Active Tasks</p>
+      <div className="flex items- mx-4 my-2 justify-center items-center">
+        <div className="relative w-1/3">
+          <span className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+            <FaSearch className="text-gray-400" />
+          </span>
+          <input
+            type="text"
+            placeholder="Search..."
+            className="py-2 pl-10 pr-4 rounded-full focus:outline-none focus:ring focus:border-blue-300 w-full border"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </div>
+      </div>
       <div className="container mx-auto mt-10">
         <table className="min-w-full">
           <thead>
@@ -37,15 +54,19 @@ const MyTasks = () => {
           </thead>
           <tbody>
             {projects.map((project) =>
-              project.tasks.map((task) => (
-                <TableRow
-                  key={task.id}
-                  taskName={task.taskName}
-                  dueDate={task.dueDate}
-                  priority={task.priority}
-                  projectName={project.name}
-                />
-              ))
+              project.tasks
+                .filter((task) =>
+                  task.taskName.toLowerCase().includes(input.toLowerCase())
+                )
+                .map((task) => (
+                  <TableRow
+                    key={task.id}
+                    taskName={task.taskName}
+                    dueDate={task.dueDate}
+                    priority={task.priority}
+                    projectName={project.name}
+                  />
+                ))
             )}
           </tbody>
         </table>
